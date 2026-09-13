61b904a9967bfd7eddd6ceaf74ce167b5d16a11d
Zara khan <zarakhang067@gmail.com>
2026-09-13 11:20:35 -0700

    array

---
+++ array.py
@@ -0,0 +1,23 @@
+# salaries = [30000, 45000, 28000, 60000, 40000]
+# print(salaries[2])
+
+
+# prices = [500, 1200, 800, 300, 1500]
+# print(prices[3])
+# prices[3]=790
+# print(prices[3])
+
+# products = ["Laptop", "Mouse", "Keyboard"]
+# products.append("headphones")
+# print(products)
+# products.insert(2,"PC")
+# print(products)
+# products.remove("Mouse")
+# print(products)
+
+
+products = ["Laptop", "Mouse", "Keyboard", "Monitor"]
+if "Mouse" in products:
+    print("PRoduct found")
+else:
+     print("PRoduct not found")
